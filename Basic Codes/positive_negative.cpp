0164e15e53d4ffe5760708e51f15d277edc939d8
Kunal22-ai <22072000kunal@gmail.com>
2021-09-27 00:11:07 +0530

    Made soln for positive negative

---
+++ Basic Codes/positive_negative.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    if (N<0)
+
+        cout << "Negative" << endl;
+    else
+        cout << "Positive" << endl;
+    return 0;
+}
