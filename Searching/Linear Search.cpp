c417cffe4fb26d663f175c59efef3075a84fd8e2
PrashantSrivastava33 <prashant.srtv33@gmail.com>
2021-09-28 10:16:38 -0700

    Solved Linear Search

---
+++ Searching/Linear Search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[10];
+	cout<<"Enter 10 elements\n";
+	for(int i=0;i<10;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"Enter number to be searched\n";
+	int d,f=0;
+	cin>>d;
+	for(int i=0;i<10;i++)
+	{
+		if(a[i]==d)
+		{
+			cout<<"Number found at position: "<<(i+1);
+			f=1;			
+		}
+	}
+	if(f!=1)cout<<"Number not found";
+}
