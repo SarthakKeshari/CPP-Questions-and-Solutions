6c785153c853246965831426aa5646ff184002d1
PrakharRathore123 <rathoreprakhar@gmail.com>
2021-09-30 02:57:04 +0530

    Solved Max subarray

--- Arrays/1D Arrays/maxsubbarraysum.cpp
+++ Arrays/1D Arrays/maxsubbarraysum.cpp
@@ -1,10 +1,12 @@
+//Find the contiguous sub-array which has the maximum sum.
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+//maxsubarraysum function to find max sum
 int maxsubarraysum(int arr[],int n)
 {
-    int max_sum=0;
-    int curr_sum=INT_MIN;
+    int max_sum=0;   //initialize max_sum
+    int curr_sum=INT_MIN;   //initialize curr_sum=INT_MIN to overcome negative values.
     for(int i=0;i<n;i++)
     {
         curr_sum=curr_sum+arr[i];
@@ -20,6 +22,7 @@ int maxsubarraysum(int arr[],int n)
     return max_sum;
 }
 
+//Driver program to test maxsubarraysum
 int main()
 {
     int arr[]={2,8,9,-8,1,9,10,8,-7};
