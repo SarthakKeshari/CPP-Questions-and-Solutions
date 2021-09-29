88bfc94ca7d5aaaada78039324073f8196d9381a
KodeInt <aksh.raizada@gmail.com>
2021-09-29 18:41:43 +0530

    solved digit name

---
+++ Basic Codes/digit_names.cpp
@@ -0,0 +1,55 @@
+//Write a program to display digit name
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a = 1;  
+    if(a == 1)
+    {
+        cout << "one";
+        
+    }
+    else if(a == 2)
+    {
+        cout << "two";
+    }
+    else if(a == 3)
+    {
+        cout << "three";
+    }
+    else if(a == 4)
+    {
+        cout << "four";
+    }
+    else if(a == 5)
+    {
+        cout << "five";
+    }
+    else if(a == 6)
+    {
+        cout << "six";
+    }
+    else if(a == 7)
+    {
+        cout << "seven";
+    }
+    else if(a == 8)
+    {
+        cout << "eight";
+    }
+    else if(a == 9)
+    {
+        cout << "nine";
+    }
+    else if(a == 0)
+    {
+        cout << "zero";
+    }
+    else    
+    {
+        cout << "invalid input";
+    }
+    return 0;
+}
