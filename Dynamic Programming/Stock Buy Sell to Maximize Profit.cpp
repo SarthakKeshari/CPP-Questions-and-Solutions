8e9e431d8d19f04fb3b9a83f51be9d342f250059
Rajat Jain <rajatjain.jain29@gmail.com>
2021-10-01 00:47:05 +0530

    Update Stock Buy Sell to Maximize Profit.cpp

--- Dynamic Programming/Stock Buy Sell to Maximize Profit.cpp
+++ Dynamic Programming/Stock Buy Sell to Maximize Profit.cpp
@@ -1,5 +1,10 @@
 // Stock Buy Sell to Maximize Profit
 
+/* Q. ou are given an array prices where prices[i] is the price of a given stock on the ith day.
+ You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
+
+
 #include <iostream>
 using namespace std;
  
