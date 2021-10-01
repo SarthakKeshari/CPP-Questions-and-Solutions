2089cb009c974c3f33bd186b133207323bd0ea5a
Vishal Singh <71393033+vishalsinghhh@users.noreply.github.com>
2021-10-01 15:51:33 +0530

    Update Solution.cpp

--- LeetCode/Interger To Roman/Solution.cpp
+++ LeetCode/Interger To Roman/Solution.cpp
@@ -4,13 +4,16 @@ using namespace std;
 class Solution
 {
 public:
+    // Member function will return the anwser of string datatype
     string intToRoman(int number)
     {
+        // Declaring an integer array of all type of possible numbers to convert it into Roman.
         int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        // Declearing a string array which will contain all type of possible Romans 
         string s[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
         int i = 12;
-        string ans = "";
-        while (number > 0)
+        string ans = "";    // Taking default answer as an empty string 
+        while (number > 0)  // initiates the while loop.
         {
             int div = number / num[i];
             number = number % num[i];
@@ -20,12 +23,14 @@ public:
             }
             i--;
         }
-        return ans;
+        return ans; // Returning the finial answer
     }
 };
 
+// { Driver Code Starts.
 int main()
 {
+    //calling method intToRoman() with number.
     Solution s;
     s.intToRoman(3);
 
