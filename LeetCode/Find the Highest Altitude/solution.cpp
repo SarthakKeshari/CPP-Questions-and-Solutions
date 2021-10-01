fb0c0434ee7d99b1ac7e6896c44684a75638563f
Mageshkrishna R <magesh12.2002@gmail.com>
2021-10-01 12:37:12 +0530

    Update solution.cpp

--- LeetCode/Find the Highest Altitude/solution.cpp
+++ LeetCode/Find the Highest Altitude/solution.cpp
@@ -3,12 +3,17 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
+        //maximum altitude
         int ans = 0;
+        //current altitude
         int altitude = 0;
+        
         for(int i: gain){
             altitude += i;
+            //it checks if altitude is greater than max altitude(i.e., ans) if so, then variable ans is updated.
             ans = max(ans, altitude);
         }
+        
         return ans;
     }
 };
