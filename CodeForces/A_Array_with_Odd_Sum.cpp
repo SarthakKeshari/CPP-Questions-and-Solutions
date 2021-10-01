3b88df9d64f4dcdcfe8b92f572579eaf291ff7a6
Abhicoder03 <choudharyab39@gmail.com>
2021-10-01 15:36:00 +0530

    Code Commented

--- CodeForces/A_Array_with_Odd_Sum.cpp
+++ CodeForces/A_Array_with_Odd_Sum.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 void solve()
 {
-    int n,a[n];
+    int n; // n is number of test cases.
 
     cin>>n;
-    int sum = 0;
+
+    int a[n]; // array of size n.
+
+    int sum = 0; // sum variable will store sum of elements of array.
     for (int i = 0; i<n; i++)
     {
         
         cin>>a[i];
 
-        sum += a[i];
+        sum += a[i]; // this is storing sum of elements.
     }
-    if (sum % 2 == 0)
+    if (sum % 2 == 0) // if value of sum of elements of array is odd then output will be no.
     {
         cout<<"NO"<<endl;
     }
-    else{
+    else{ // and if value of sum of elements of array is even then output will be yes
         cout<<"YES"<<endl;
     }
     
