6cc9e1cac0f578023729c8bc268120869e656cc9
Prasoon Jain <80613141+PrasoonJain2002@users.noreply.github.com>
2021-10-02 01:32:20 +0530

    Update Xs_and_Os.cpp

--- Coding Competition/Xs_and_Os.cpp
+++ Coding Competition/Xs_and_Os.cpp
@@ -13,46 +13,46 @@ int main(){
         for(int i=0;i<n;i++)
             for(int j=0;j<n;j++)
                 cin>>a[i][j];//inputting arrays
-        ll c[2*n]={0},f=0,co=0,cd=0;//variables for count of 'O' and '.'
-        for(int i=0;i<n;i++){
+        ll c[2*n]={0},f=0,co=0,cd=0;//variables for count of 'O' and '.' and array c for empty spaces both row wise and column wise
+        for(int i=0;i<n;i++){// checking matrix row wise
             co=0,cd=0;
             for(int j=0;j<n;j++){
-                if(a[i][j]=='O')
+                if(a[i][j]=='O')//counting 'O'
                     co++;
-                if(a[i][j]=='.')
+                if(a[i][j]=='.')//counting '.'
                     cd++;
             }
-            if(co==0)
-                c[f]=cd;
+            if(co==0)// if there are no 'O' i.e. there are only 'X" and '.'
+                c[f]=cd;//storing how many empty spaces are there
             f++;
         }
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n;j++){//checking matrix column wise
             co=0,cd=0;
             for(int i=0;i<n;i++){
-                if(a[i][j]=='O')
+                if(a[i][j]=='O')//counting 'O'
                     co++;
-                if(a[i][j]=='.')
+                if(a[i][j]=='.')//counting '.'
                     cd++;
             }
-            if(co==0)
-                c[f]=cd;
+            if(co==0)// if there are no 'O' i.e. there are only 'X" and '.'
+                c[f]=cd;//storing how many empty spaces are there
             f++;
         }
         sort(c,c+(2*n));//sorting of array in ascending order using STL
         ll first=0,min,count=0;
         for(int i=0;i<2*n;i++){
-            if(c[i]>0&&first==0){
-                min=c[i];
+            if(c[i]>0&&first==0){//checking for first number other than 0 in the sorted array
+                min=c[i];//storing the minimum number
                 first=1;
             }
-            if(c[i]==min&&first==1)
+            if(c[i]==min&&first==1)//counting how many times that first minimum number is coming in array c
                 count++;
         }
         if(min==1&&count>0){
             for(int i=0;i<n;i++)
                 for(int j=0;j<n;j++){
                     if(a[i][j]=='.'){
-                        int a1=0,b=0;
+                        int a1=0,b=0;//counter to check along both row wise and column wise
                         for(int k=0;k<n;k++){//checking for different conditions of pattern of 'X','O' and '.'
                             if(a[i][k]=='O')
                                 a1++;
