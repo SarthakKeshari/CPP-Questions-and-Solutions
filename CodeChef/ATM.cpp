01784d867e617085fe43947da2ad546c855f03bf
Sejal Koshta <86724161+sej28@users.noreply.github.com>
2021-10-01 23:20:42 +0530

    Create ATM.cpp
    Adding ATM problem from CodeChef

---
+++ CodeChef/ATM.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int withdraw;
+    double balance;
+
+    cin >> withdraw >> balance;
+
+    if (withdraw % 5 == 0 && withdraw + 0.5 <= balance)
+    {
+        cout << balance - withdraw - 0.5 << endl; 
+    }
+    else
+    {
+        cout << balance;
+    }
+
+    return 0;
+}
