aa6b1ef195679c03a52a9fcd9d21ac786f35fd0c
Rustzz-27 <reshuag19@gmail.com>
2021-10-01 18:08:23 +0700

    added comments

--- LeetCode/Island perimeter/solution.cpp
+++ LeetCode/Island perimeter/solution.cpp
@@ -1,8 +1,10 @@
+//https://leetcode.com/problems/island-perimeter/
+
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int result=0;
-        int row = grid.size(), col = grid[0].size();
+        int row = grid.size(), col = grid[0].size(); // row and column
         if(row==0 || col==0)
             return 0;
         for(int i=0;i<row;i++)
@@ -11,13 +13,14 @@ public:
             {
                 if(grid[i][j]==1)
                 {
+                /*  if we go out of the box or there is water,  then only we can found perimeter */
                     result+=4;
-                    if(j>0 && grid[i][j-1]==1)
+                    if(j>0 && grid[i][j-1]==1)   // chek if has left block
                     {
                         result=result-2;
                     }
                         
-                    if(i>0 && grid[i-1][j]==1)
+                    if(i>0 && grid[i-1][j]==1)  // check if it has up block
                     {
                         result=result-2;
                     }
@@ -28,3 +31,4 @@ public:
         return result;
     }
 };
+
