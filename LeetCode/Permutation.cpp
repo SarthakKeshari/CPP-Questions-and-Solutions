71ee603de506c20635ac6b965cbb95e0a89ea248
Smitarani Pradhan <54846994+smitaranii@users.noreply.github.com>
2021-10-02 20:24:14 +0530

    Added solution to Permutation

---
+++ LeetCode/Permutation.cpp
@@ -0,0 +1,25 @@
+//Problem link:https://leetcode.com/problems/permutations/
+class Solution {
+public:
+  void backtrack(vector<int>& nums,vector<vector<int>>& result,int st){
+      if(st>=nums.size())
+      {
+          result.push_back(nums);
+          return;
+      }
+      for(int i=st;i<nums.size();i++)
+      {
+          swap(nums[st],nums[i]);
+          backtrack(nums,result,st+1);
+          swap(nums[st],nums[i]);
+      }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+         vector<vector<int>> result;
+        backtrack(nums,result,0);
+        return result;
+    }
+};
