1647ef475112f69ef72acda2efe98ce2838a4703
B.Hariprasad <hariprasad19036@cse.ssn.edu.in>
2021-10-02 23:47:13 +0530

    Added Solution in cpp to Higher Order Functions problem from codeforces

--- CodeForces/Higher_Order_Functions.cpp
+++ CodeForces/Higher_Order_Functions.cpp
@@ -1,3 +1,44 @@
+// Problem statement
+/*
+Helen studies functional programming and she is fascinated with a concept of higher order functions — functions that are taking other functions as parameters. She decides to generalize the concept of the function order and to test it on some examples.
+
+For her study, she defines a simple grammar of types. In her grammar, a type non-terminal T is defined as one of the following grammar productions, together with order(T), defining an order of the corresponding type:
+
+"()" is a unit type, order("()")=0.
+"(" T ")" is a parenthesized type, order("("T")")=order(T).
+T1 "->" T2 is a functional type, order(T1"->"T2)=max(order(T1)+1,order(T2)). The function constructor T1 "->" T2 is right-to-left associative, so the type "()->()->()" is the same as the type "()->(()->())" of a function returning a function, and it has an order of 1. While "(()->())->()" is a function that has an order-1 type "(()->())" as a parameter, and it has an order of 2.
+Helen asks for your help in writing a program that computes an order of the given type.
+
+Input
+The single line of the input contains a string consisting of characters '(', ')', '-', and '>' that describes a type that is valid according to the grammar from the problem statement. The length of the line is at most 104 characters.
+
+Output
+Print a single integer — the order of the given type.
+
+Examples
+input
+()
+output
+0
+input
+()->()
+output
+1
+input
+()->()->()
+output
+1
+input
+(()->())->()
+output
+2
+input
+()->(((()->())->()->())->())
+output
+3 
+*/
+
+// Solution
 #include <bits/stdc++.h>
 using namespace std;
 
