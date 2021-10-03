f17a34b46288e43ca49f0e33bede9569df41ba3d
suranaakshat02 <suranaakshat02@gmail.com>
2021-10-03 20:08:35 +0530

    Added Butterfly pattern.cpp and Evenodd.cpp

---
+++ Basic Codes/Evenodd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+    cout<<"Enter the value of num: ";
+	cin>>num;
+    if(num%2==0){
+        cout<<"EVEN Number";
+    }
+    else 
+    cout<<"ODD Number";
+    return 0;
+}
