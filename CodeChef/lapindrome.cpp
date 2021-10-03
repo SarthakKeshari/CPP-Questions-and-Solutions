c724bbcf9a752b325946456527a4d804e74a70cb
zafer247 <zafershah247@gmail.com>
2021-10-03 13:52:03 +0530

    Codechef Lapindrome problem added Issue no 172

--- CodeChef/lapindrome.cpp
+++ CodeChef/lapindrome.cpp
@@ -1,3 +1,8 @@
+
+/*Lapindrome is defined as a string which when split in the middle, gives two halves having the same characters and same frequency of each character. If there are odd number of characters in the string, we ignore the middle character and check for lapindrome. For example gaga is a lapindrome, since the two halves ga and ga have the same characters with same frequency. Also, abccab, rotor and xyzxy are a few examples of lapindromes. Note that abbaab is NOT a lapindrome. The two halves contain the same characters but their frequencies do not match.
+Your task is simple. Given a string, you need to tell if it is a lapindrome.*/
+
+
 #include <iostream>
 using namespace std;
 
