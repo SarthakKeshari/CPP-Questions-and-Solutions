c87f4d7bed48f9769fddcd0216d25378c88e7430
Ujjawal Sharma <64632969+ujju20@users.noreply.github.com>
2021-10-05 11:46:25 +0530

    Update Solution.cpp

--- LeetCode/Power-of-four/Solution.cpp
+++ LeetCode/Power-of-four/Solution.cpp
@@ -6,18 +6,17 @@ bool isPowerOfFour(int n) {
         
         if(n<=0)
         {
-            return false;
+            return false;     //Negative Numbers can't be a power of 4
         }
-        return !(n&(n-1)) and n%3==1;
-        
+        return !(n&(n-1)) and n%3==1;   //If a number is power of 2 then n7(n-1) is zero and if the number is power of 4 it always gives 1 when taken mod with 3        
         
         
     }
 int main() {
     int n;
-    cin>>n;
+    cin>>n;  //Taking Input the number
     bool ans=isPowerOfFour(n);
-    if(ans) 
+    if(ans)        
     {
         cout<<"Yes "<<n<<" is a Power of 4";
     }
