4cd4e811f6e9c0c49541cd65b4ad3653a7c66fc6
Ilim Ugur <ilimugur@gmail.com>
2021-10-07 14:14:39 +0300

    Add comment and mention the problem.

--- Pattern printing/Letter_B.cpp
+++ Pattern printing/Letter_B.cpp
@@ -1,3 +1,21 @@
+/*
+This code prints the letter B, as shown below.
+
+**#####
+**     ##
+**      ##
+**     ##
+**#####
+**     ##
+**      ##
+**     ##
+**#####
+ 
+There are 3 distinct patterns featured in printing the letter B to console.
+If we define those patterns as strings, and also define the order in which
+they are printed, then we could iterate over the order and print the correct
+pattern, resulting in the correct output. (i.e., the letter B)
+*/
 #include <iostream>
 
 int main()
