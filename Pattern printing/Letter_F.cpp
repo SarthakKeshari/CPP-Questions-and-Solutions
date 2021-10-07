01cbb8e0ca32ed3309467b4d66d2159e647d4ee1
Ankita Kumari <59385327+Blankita21@users.noreply.github.com>
2021-10-07 19:59:16 +0530

    Update Letter_F.cpp

--- Pattern printing/Letter_F.cpp
+++ Pattern printing/Letter_F.cpp
@@ -14,27 +14,26 @@
 using namespace std;
 
 int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
     // Printing the pattern
 
+    // As the pattern is of 9x9 size, therefore a nested for-loop from 0-8 is peformed.
     for (int i = 0; i < 9; i++)
     {
         for (int j = 0; j < 9; j++)
         {
+            // To print the first two # columns.
             if (j < 2)
             {
                 cout << "#";
             }
 
+            // To print the $ rows.
             else if ((i == 0 or i == 4) and j >= 2)
             {
                 cout << "$";
             }
 
+            // To print the empty spaces.
             else
             {
                 cout << " ";
