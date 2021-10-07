abe69c6ceb47966a3422ed3d37233aff2b5e01dc
Kaushik Deka <89312403+KAUSHIK-DEKA@users.noreply.github.com>
2021-10-07 21:55:20 +0530

    Update doublylinkiedlist.c

--- GeeksForGeeks/doublylinkiedlist.c
+++ GeeksForGeeks/doublylinkiedlist.c
@@ -15,23 +15,28 @@ LinkedList, traversal, pointers
 
 #include<stdlib.h>
 #include<stdio.h>
+//node creation
 struct node{
     int data;
-    struct node* prev;
-    struct node* next;
+    struct node* prev;//stores address of previous node
+    struct node* next;//stores address of next node
 };
+//assigning head as a global so that it can bes used by all the functions and we dont have to specifically call for it everytime
 struct node* head;
+//function to create a new node
 struct node* getnewNode(int x)
-{
+{   //creating a newnode in the heap and assigning data, prev and next to it
     struct node* newNode= (struct node*)malloc(sizeof(struct node));
     newNode->data=x;
     newNode->prev=NULL;
     newNode->next=NULL;
     return newNode;
 }
+// function to enter a new element from the head of the list
 void insertHead(int x)
 {
     struct node* temp=getnewNode(x);
+    //if the list is empty
     if(head==NULL)
     {
         head=temp;
@@ -41,10 +46,12 @@ void insertHead(int x)
     temp->next=head;
     head=temp;
 }
+// function to enter a new element from the tail of the list
 void insertTail(int x)
 {
     struct node* ptr=getnewNode(x);
     struct node* temp;
+    //if the list is empty
     if(head==NULL)
     {
         head=ptr;
@@ -59,9 +66,11 @@ void insertTail(int x)
     ptr ->prev=temp;
     ptr->next = NULL;
 }
+// forward printing
 void printforward()
 {
     struct node* temp=head;
+    //iterating till the ned of the list and printing the values as we go along
     while(temp!=NULL)
     {
         printf("%d ",temp->data);
@@ -72,12 +81,15 @@ void printforward()
 void printreverse()
 {
     struct node* temp=head;
+    //for empty list
     if(temp==NULL)
         return;
+    //traversing towards the and of the list
     while(temp->next!=NULL)
     {
         temp=temp->next;
     }
+    // as we reached the last node. we will traverse backwards and print the data as we go along
      while(temp!=NULL)
     {
         printf("%d ",temp->data);
@@ -85,6 +97,7 @@ void printreverse()
     }
     printf("\n");
 }
+//driver code
 int main()
 {
   head=NULL;
