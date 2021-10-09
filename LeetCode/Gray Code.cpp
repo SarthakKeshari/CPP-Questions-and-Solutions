ea606efb7be55e112075930f1ca04b4933d0155c
Diwakar Singh <66394899+diwakarsingh18277@users.noreply.github.com>
2021-10-09 16:29:29 +0530

    Update Gray Code.cpp

--- LeetCode/Gray Code.cpp
+++ LeetCode/Gray Code.cpp
@@ -17,31 +17,32 @@ Given an integer n, return any valid n-bit gray code sequence.
 using namespace std;
 
 class Solution {
-
+    // Recursive function
     void recur(int num, int pos, vector<int>& ans) {
-        ans.push_back(num);
-        if (pos < 0) return;
-
+        ans.push_back(num); // inserting the number in answer vector
+        if (pos < 0) return; // if position is less than zero then no need to furthur call the function
+        
+        // setting the bits of number from pos to zero and calling the function recursively
         for (int i = pos; i >= 0; i--)
-            recur((num | (1 << i)), i - 1, ans);   // num | (1 << i) will set the i th
+            recur((num | (1 << i)), i - 1, ans);   // num | (1 << i) will set the i th bit
     }
 
   public:
     vector<int> grayCode(int n) {
-        vector<int> ans;
-        recur(0, n - 1, ans);
+        vector<int> ans; // defining the answer vector 
+        recur(0, n - 1, ans); // calling the recursive function
 
-        return ans;
+        return ans; // return the answer vector
     }
 };
 
 
 int32_t main() {
-    Solution solution;
+    Solution solution; // creating a object of solution class
 
     int n;
-    cin >> n;
-    vector<int> ans = solution.grayCode(n);
+    cin >> n; // taking the input 
+    vector<int> ans = solution.grayCode(n); // calling the grayCode function of Solution class
 
     for (auto j : ans)
         cout << j << ' ';
