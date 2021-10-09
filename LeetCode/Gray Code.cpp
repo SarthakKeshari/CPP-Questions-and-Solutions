286f813689d1cb334aed3832a90defd89e4c71f1
Diwakar Singh <66394899+diwakarsingh18277@users.noreply.github.com>
2021-10-09 14:11:21 +0530

    Update Gray Code

--- LeetCode/Gray Code.cpp
+++ LeetCode/Gray Code.cpp
@@ -1,29 +1,18 @@
 class Solution {
 
     void recur(int num, int pos, vector<int>& ans) {
-
         ans.push_back(num);
-
         if (pos < 0) return;
-
         
-
         for (int i = pos; i >= 0; i--)
-
             recur((num | (1 << i)), i - 1, ans);   // num | (1 << i) will set the i th 
-
     }
 
 public:
-
     vector<int> grayCode(int n) {
-
         vector<int> ans;
-
         recur(0, n - 1, ans);
-
+        
         return ans;
-
     }
-
 };
