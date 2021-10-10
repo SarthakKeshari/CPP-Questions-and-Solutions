0e89244c8407a3ecf41b4fc62558c0278af33a78
Rashi Shukla <87816619+17rashi@users.noreply.github.com>
2021-10-10 14:40:30 +0530

    Added question 3

---
+++ Greedy Algorithms/question-3_choosingTeams.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n,k;
+   cin>>n>>k;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+      cin>>arr[i];
+   }
+   int ans=0;
+   for(int i=0;i<n;i++)
+   {
+      if(arr[i]+k<=5)
+      {
+         ans++;
+      }
+   } 
+   cout<<ans/3<<endl;  
+  return 0;
+}
