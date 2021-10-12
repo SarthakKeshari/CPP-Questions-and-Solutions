3441f18adf24f837b40f0507cd75277a0e491b92
anshuman725 <anshuman.par@gmail.com>
2021-10-12 12:03:34 +0530

    Adding the question and the comments

--- GeeksForGeeks/Even-after-Odd-LinkedList.cpp
+++ GeeksForGeeks/Even-after-Odd-LinkedList.cpp
@@ -1,5 +1,40 @@
+/*For a given singly linked list of integers, arrange the elements such that all the even numbers are placed after the odd numbers. The relative order of the odd and even terms should remain unchanged.
+Note :
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format:
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space. 
+ Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format:
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+1 4 5 2 -1
+Sample Output 1 :
+1 5 4 2 
+Sample Input 2 :
+2
+1 11 3 6 8 0 9 -1
+10 20 30 40 -1
+Sample Output 2 :
+1 11 3 9 6 8 0
+10 20 30 40 
+*/
+
+
 #include <iostream>
 using namespace std;
+// Defining Node Class
 class Node
 {
 public:
@@ -12,6 +47,7 @@ public:
     }
 };
 
+// Taking Input 
 Node *takeinput()
 {
     int data;
@@ -35,6 +71,7 @@ Node *takeinput()
     return head;
 }
 
+// Function to Print the List
 void print(Node *head)
 {
     Node *temp = head;
@@ -68,6 +105,8 @@ Node *evenAfterOdd(Node *head)
     //write your code here
     if (head == NULL)
         return head;
+
+    // Taking 4 node pointer's
     Node *oddh = NULL;
     Node *oddt = NULL;
     Node *evenh = NULL;
@@ -75,6 +114,7 @@ Node *evenAfterOdd(Node *head)
 
     while (head != NULL)
     {
+        // checking if node is even or odd
         if (head->data % 2 != 0)
         {
             if (oddh == NULL && oddt == NULL)
@@ -103,6 +143,8 @@ Node *evenAfterOdd(Node *head)
         }
         head = head->next;
     }
+
+    // merging both linked list 
     if (oddh == NULL)
     {
         event->next = NULL;
