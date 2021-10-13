7774dd1fa5f509f1b49acbdba71dbabc75589426
Harshit Poddar <89685284+Harshit-Poddar90@users.noreply.github.com>
2021-10-13 10:59:43 +0530

    Update For Loop.cpp

--- Hackerrank/For Loop/For Loop.cpp
+++ Hackerrank/For Loop/For Loop.cpp
@@ -1,32 +1,43 @@
-//QUESTION:- 
+// QUESTION:- 
 
-//A for loop is a programming language statement which allows code to be repeatedly executed.
+// A for loop is a programming language statement which allows code to be repeatedly executed.
 
-//The syntax is
+// The syntax is
 
-//for ( <expression_1> ; <expression_2> ; <expression_3> )
+// for ( <expression_1> ; <expression_2> ; <expression_3> )
  //   <statement>
-//expression_1 is used for intializing variables which are generally used for controlling the terminating flag for the loop.
-//expression_2 is used to check for the terminating condition. If this evaluates to false, then the loop is terminated.
-//expression_3 is generally used to update the flags/variables.
-//A sample loop is
+// expression_1 is used for intializing variables which are generally used for controlling the terminating flag for the loop.
+// expression_2 is used to check for the terminating condition. If this evaluates to false, then the loop is terminated.
+// expression_3 is generally used to update the flags/variables.
+// A sample loop is
 
-//for(int i = 0; i < 10; i++) {
+// for(int i = 0; i < 10; i++) {
 //    ...
-//}
-//In this challenge, you will use a for loop to increment a variable through a range.
+// }
+// In this challenge, you will use a for loop to increment a variable through a range.
 
 
-//SOLUTION:-   For loop based.
+
+// Logic:-  You will be given two positive integers, a and b (a<=b), separated by a newline.
+
+//            For each integer n, in the interval [a,b]:
+//            If 1<=n<=9 then print the English representation of it in lowercase. That is "one" for 1, "two" for 2, and so on.
+//            Else if n>9 and it is an even number, then print "even".
+//            Else if n>9 and it is an odd number, then print "odd".
+
+
+
+
+// SOLUTION:-   For loop based.
 
 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
+{   // Variable a,b
     int a,b;
     string c[]={"","one","two","three","four","five","six","seven","eight","nine"};
-    cin>>a>>b;
-    for(int i=a;i<=b;i++)
-        cout<<((i<=9)?c[i]:((i%2==0)?"even":"odd"))<<endl;
+   cin>>a>>b; //Here cpp takes users value and run it.
+    for(int i=a;i<=b;i++) // Here for loop starts.
+        cout<<((i<=9)?c[i]:((i%2==0)?"even":"odd"))<<endl; //At this point answer is reflected.
 }
