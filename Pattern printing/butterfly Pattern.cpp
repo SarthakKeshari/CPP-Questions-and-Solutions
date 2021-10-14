ed68874a38245eca00dcfe406c9dc156c9abfbce
suranaakshat02 <suranaakshat02@gmail.com>
2021-10-14 20:47:00 +0530

    updated butterfly Pattern.cpp

--- Pattern printing/butterfly Pattern.cpp
+++ Pattern printing/butterfly Pattern.cpp
@@ -4,33 +4,33 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    for(int i=1;i<=n;i++){
+    cout<<"Enter the value of n: ";      
+    cin>>n;                               // taken a input from user
+    for(int i=1;i<=n;i++){                //used for loop for *
         for(int j=1;j<=i;j++){
-           cout<<"*"; 
+           cout<<"*";                     //printed *
         }
-        int space=2*n-2*i;
-        for(int j=1;j<=space;j++){
-           cout<<" ";
+        int space=2*n-2*i;               
+        for(int j=1;j<=space;j++){        // used for loop for Space
+           cout<<" ";                     //printed space
         }
-        for(int j=1;j<=i;j++){
-           cout<<"*";
+        for(int j=1;j<=i;j++){            //again used for loop for *
+           cout<<"*";                     // printed *
         }
-    cout<<endl;    
+    cout<<endl;                           // line change
     }
-    for(int i=n;i>=1;i--){
-        for(int j=i;j>=1;j--){
+    for(int i=n;i>=1;i--){                //used for loop for *
+        for(int j=i;j>=1;j--){            //printed *
            cout<<"*"; 
         }
         int space=2*n-2*i;
-        for(int j=space;j>=1;j--){
-           cout<<" ";
+        for(int j=space;j>=1;j--){        // used for loop for Space
+           cout<<" ";                     //printed space
         }
-        for(int j=i;j>=1;j--){
-           cout<<"*";
+        for(int j=i;j>=1;j--){            //again used for loop for *
+           cout<<"*";                     //printed *
         }
-    cout<<endl;    
+    cout<<endl;                           // line change
     }  
     return 0;
 }
