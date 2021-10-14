10f0a35fc0a539df5348a3bda4af43861ab2dc4f
Sanyam Jain <86163358+jainsanyam228@users.noreply.github.com>
2021-10-14 20:52:34 +0530

    Update Registration_system.cpp

--- CodeForces/Registration_system.cpp
+++ CodeForces/Registration_system.cpp
@@ -1,4 +1,15 @@
-// SANYAM JAIN //
+
+//C.Registration system
+// que link --> https://codeforces.com/problemset/problem/4/C
+/*A new e-mail service "Berlandesk" is going to be opened in Berland in the near future. The site administration wants
+to launch their project as soon as possible, that's why they ask you to help. You're suggested to implement the prototype 
+of site registration system. The system should work on the following principle.
+
+Each time a new user wants to register, he sends to the system a request with his name. If such a name does not exist in the system database, it is inserted into 
+the database, and the user gets the response OK, confirming the successful registration. If the name already exists in the system database, the system makes up a 
+new user name, sends it to the user as a prompt and also inserts the prompt into the database. The new name is formed by the following rule. Numbers, starting with
+1, are appended one after another to name (name1, name2, ...), among these numbers the least i is found so that namei does not yet exist in the database.
+*/
 #include <bits/stdc++.h>
 #include<string>
 #include<unordered_map>
