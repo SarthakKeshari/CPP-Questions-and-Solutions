04ab5e1f49da3d2752af1986656d2410414a299b
kavya1902 <kpaliwal2002@gmail.com>
2021-10-18 00:46:09 +0530

    ReverseInteger

--- LeetCode/ReverseInteger/reverseInteger.cpp
+++ LeetCode/ReverseInteger/reverseInteger.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution{
     public:
+    // Member function will return the anwser of int datatype
         int reverse(int x){
             int rev=0;
             while(x!=0){
@@ -15,12 +16,14 @@ class Solution{
                 if (rev < -2147483647/10){
                     return 0;
                 }rev=rev*10+digit;
+                // Returning the finial answer
             }return rev;
         }
 };
-
+// { Driver Code Starts.
 int main(){
     Solution s;
+    //calling method reverseI() with number.
     s.reverse(4);
 
     return 0;
