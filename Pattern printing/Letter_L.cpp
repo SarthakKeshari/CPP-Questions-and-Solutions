849b230f1131ad6a0d18c63b3898a8d46a1837a6
Anova07 <65520755+Anova07@users.noreply.github.com>
2021-10-18 00:57:39 +0530

    Create Letter_L.cpp

---
+++ Pattern printing/Letter_L.cpp
@@ -0,0 +1,26 @@
+/*
+This code prints the letter L, as shown below.
+
+##
+##
+##
+##
+##
+##
+##
+##&&&&&&&
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+	for(int i=0; i < 7; ++i)
+	{
+		printf("##\n");
+	}
+printf("##&&&&&&&\n");
+	return 0;
+}
