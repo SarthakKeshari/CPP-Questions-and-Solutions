24405b450d6a39114f31925dce3655445bc3aed4
MEGHWANT SINGH <68533246+Meghwantsingh@users.noreply.github.com>
2021-10-17 09:41:11 +0530

    Update palindromeNumber.cpp
    pls review.

--- LeetCode/palindromeNumber.cpp
+++ LeetCode/palindromeNumber.cpp
@@ -1,4 +1,12 @@
+/*Given an integer x, return true if x is palindrome integer.
 
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+Example 1:
+
+Input: x = 121
+Output: true
+*/
 class Solution {
 public:
     bool isPalindrome(int x) {          // function to return true or false
