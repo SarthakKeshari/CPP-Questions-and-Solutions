3da6bc9712352296d2096d282348c38a5ce4ce4c
Sejal Koshta <86724161+sej28@users.noreply.github.com>
2021-10-18 15:26:15 +0530

    Add All_or_any.cpp

---
+++ Arrays/1D Arrays/All or any/All_or_any.cpp
@@ -0,0 +1,73 @@
+/*You are given a space-separated list of integers. If all the integers are positive, then you need to check if any integer is a palindromic integer.
+Print "True" if all the conditions of the problem statement are satisfied. Otherwise, print "False".
+Conditions--
+
+1. All the integers are positive.
+2. At least one integer is a palindrome, note that all the single-digit numbers are palindrome.            */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int n;
+  cout << "Enter the number of inputs you want to check: ";
+  cin >> n;
+  int s[n];
+  int counter = 0;
+  int pos = 0;
+  for (int i = 0; i < n; i++)
+    {
+      cin >> s[i];
+    }
+
+
+
+  for (int j = 0; j < n; j++)
+    {
+      if (s[j] > 0)
+	{
+	  pos = pos + 1;
+	  int p, q, num;
+	  q = s[j];
+	  p = s[j];
+	  num = s[j];
+	  int y = 0;
+      int d = 0;
+	  
+	      while (p > 0)
+		{
+		  d = p % 10;
+		  y = (y * 10) + d;
+          p = p / 10;
+		}
+		
+	      if (num == y)
+		{
+		  counter = counter + 1;
+		}
+	    y = 0;
+	}
+	
+    }
+
+  if (pos == n)
+    {
+      if (counter >= 1)
+	  {
+	    cout << "True";
+	  }
+      else
+	  {
+	    cout << "False";
+	  }
+    }
+  else
+    {
+      cout << "False";
+    }
+
+  return 0;
+}
