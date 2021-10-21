d4361a437c1d9ea1f0cb26aff916f0a92d4b437f
YashGosaliya <58128160+YashGosaliya@users.noreply.github.com>
2021-10-21 20:46:11 +0530

    Update trappingRainWater.cpp

--- LeetCode/trappingRainWater.cpp
+++ LeetCode/trappingRainWater.cpp
@@ -7,27 +7,31 @@ public:
         
         int n = height.size();
         int total=0;
-        int left = 0, right = n-1;
-        int maxLeft=0, maxRight=0;
-        while(left <= right){
-            if(height[left]<=height[right]){
-                if(height[left] >= maxLeft)
+        int left = 0, right = n-1; //Taking first element as left and last element as right
+        
+        int maxLeft=0, maxRight=0;  //Consider max height for left and right respectively
+        
+        while(left <= right){   //Check till left element is small or equal to right
+            
+            if(height[left]<=height[right]){  //check till height for left is less or equal to right
+                
+                if(height[left] >= maxLeft)   //Height at Left is greater or equal to maximum value at left
                     maxLeft = height[left];
                 else
-                    total += maxLeft - height[left];
+                    total += maxLeft - height[left]; //Add the difference between maximum value and height at left
                 
-                left++;
+                left++; //Increment from left side
             }
             else{
-                if(height[right] >= maxRight){
+                if(height[right] >= maxRight){  //Height at Right is greater or equal to maximum value at right
                     maxRight = height[right];
                 }
                 else
-                    total += maxRight - height[right];
+                    total += maxRight - height[right]; //Add the difference between maximum value and height at right
                 
-                right--;
+                right--; //Decrement from right side
             }
         }
-        return total;
+        return total;  //Return the total units of water trapped between bars
     }
 };
