ea3d93f64b38fe22c89ef12962b3a5d7aff1d15d
YashGosaliya <58128160+YashGosaliya@users.noreply.github.com>
2021-10-21 09:59:17 +0530

    Create trappingRainWater.cpp

---
+++ LeetCode/trappingRainWater.cpp
@@ -0,0 +1,33 @@
+
+//https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n = height.size();
+        int total=0;
+        int left = 0, right = n-1;
+        int maxLeft=0, maxRight=0;
+        while(left <= right){
+            if(height[left]<=height[right]){
+                if(height[left] >= maxLeft)
+                    maxLeft = height[left];
+                else
+                    total += maxLeft - height[left];
+                
+                left++;
+            }
+            else{
+                if(height[right] >= maxRight){
+                    maxRight = height[right];
+                }
+                else
+                    total += maxRight - height[right];
+                
+                right--;
+            }
+        }
+        return total;
+    }
+};
